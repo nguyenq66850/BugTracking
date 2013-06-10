97b4ff944aaa484909780423dd705f3ebb67bfd2
Quan T. Nguyen <qnguyen@tradestation.com>
2013-06-10 11:23:21 -0500

    Version 1.0

---
+++ BugTracking/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BugTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bugnumLabel = new System.Windows.Forms.Label();
+            this.bugnumTBox = new System.Windows.Forms.TextBox();
+            this.previewPanel = new System.Windows.Forms.RichTextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.blockLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bugnumLabel
+            // 
+            this.bugnumLabel.AutoSize = true;
+            this.bugnumLabel.Location = new System.Drawing.Point(30, 30);
+            this.bugnumLabel.Name = "bugnumLabel";
+            this.bugnumLabel.Size = new System.Drawing.Size(66, 13);
+            this.bugnumLabel.TabIndex = 0;
+            this.bugnumLabel.Text = "Bug Number";
+            // 
+            // bugnumTBox
+            // 
+            this.bugnumTBox.Location = new System.Drawing.Point(100, 27);
+            this.bugnumTBox.Name = "bugnumTBox";
+            this.bugnumTBox.Size = new System.Drawing.Size(100, 20);
+            this.bugnumTBox.TabIndex = 1;
+            this.bugnumTBox.Text = "38868";
+            // 
+            // previewPanel
+            // 
+            this.previewPanel.Location = new System.Drawing.Point(450, 20);
+            this.previewPanel.Name = "previewPanel";
+            this.previewPanel.Size = new System.Drawing.Size(550, 700);
+            this.previewPanel.TabIndex = 2;
+            this.previewPanel.Text = "";
+            this.previewPanel.WordWrap = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(270, 25);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(350, 25);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File";
+            // 
+            // blockLabel
+            // 
+            this.blockLabel.AutoSize = true;
+            this.blockLabel.Location = new System.Drawing.Point(150, 60);
+            this.blockLabel.Name = "blockLabel";
+            this.blockLabel.Size = new System.Drawing.Size(70, 13);
+            this.blockLabel.TabIndex = 6;
+            this.blockLabel.Text = "Block of lines";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1016, 741);
+            this.Controls.Add(this.blockLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.previewPanel);
+            this.Controls.Add(this.bugnumTBox);
+            this.Controls.Add(this.bugnumLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bug Tracking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bugnumLabel;
+        private System.Windows.Forms.TextBox bugnumTBox;
+        private System.Windows.Forms.RichTextBox previewPanel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label blockLabel;
+    }
+}
+
---
+++ BugTracking/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracking
+{
+    public partial class Form1 : Form
+    {
+        class ReportEntry
+        {
+            public TextBox fileTBox = new TextBox();
+            public TextBox blockTBox = new TextBox();
+            public Button previewButton = new Button();
+            public Button removeButton = new Button();
+            public int id;
+
+            public ReportEntry(int _id)
+            {
+                previewButton.Text = "Preview";
+                removeButton.Text = "Remove";
+                fileTBox.MouseDown += new MouseEventHandler(fileTBox_MouseDown);
+                previewButton.Click += new EventHandler(previewButton_Click);
+                removeButton.Click += new EventHandler(removeButton_Click);
+                id = _id;
+            }
+
+            public void UpdateLocation()
+            {
+                fileTBox.Location = new Point(30, 30 * id + 77);
+                blockTBox.Location = new Point(150, 30 * id + 77);
+                previewButton.Location = new Point(270, 30 * id + 75);
+                removeButton.Location = new Point(350, 30 * id + 75);
+            }
+
+            private void fileTBox_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    OpenFileDialog fd = new OpenFileDialog();
+                    // System.Diagnostics.Debug.WriteLine(fd != null);
+                    if (fd.ShowDialog() == DialogResult.OK)
+                    {
+                        fileTBox.Text = fd.FileName;
+                    }
+                }
+            }
+
+            public override string ToString()
+            {
+                string result = "";
+
+                // Parse the string for block of lines
+                string s = blockTBox.Text + "-";
+                string[] arr = s.Split(new Char[] { '-', ' ', ',', '.', ':', '\t' });
+                int start;
+                int end;
+                string line = "";
+
+                try
+                {
+                    start = Convert.ToInt32(arr[0]);
+                    try
+                    {
+                        end = Convert.ToInt32(arr[1]);
+                    }
+                    catch
+                    {
+                        end = start;
+                    }
+
+                    System.Diagnostics.Debug.WriteLine(String.Format("{0}-{1}", start, end));
+
+                    try
+                    {
+                        System.IO.StreamReader sr = new System.IO.StreamReader(fileTBox.Text);
+                        int i = 0;
+                        while (!sr.EndOfStream && i < start)
+                        {
+                            i++;
+                            line = sr.ReadLine();
+                        }
+
+                        result = fileTBox.Text;
+
+                        while (!sr.EndOfStream && i <= end)
+                        {
+                            result = result + String.Format("\n{0}\t{1}", i, line);
+                            i++;
+                            line = sr.ReadLine();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(String.Format("Entry {0}:\n{1}", id + 1, ex.Message));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(String.Format("Entry {0}:\nWrong number format", id + 1));
+                }
+
+                result = result + "\n\n";
+                
+                return result;
+            }
+            
+            private void previewButton_Click(object sender, EventArgs e)
+            {
+                // System.Diagnostics.Debug.WriteLine("Preview button clicked");
+                // Form parentForm = previewButton.Parent as Form1;
+                // System.Diagnostics.Debug.WriteLine(parentForm != null); // Check if the button belongs to the main form
+
+                // Tranfer control to previewPanel in the main form
+                RichTextBox previewPanel = previewButton.Parent.Controls["previewPanel"] as RichTextBox;
+                // previewPanel.AppendText("Test");
+                previewPanel.Clear();
+                previewPanel.AppendText(this.ToString());
+            }
+
+            private void removeButton_Click(object sender, EventArgs e)
+            {
+                // System.Diagnostics.Debug.WriteLine("Preview button clicked");
+
+                // Tranfer control to the main form
+                Form1 mainform = previewButton.Parent as Form1;
+
+                // Remove all components of this entry from the main form
+                mainform.Controls.Remove(fileTBox);
+                mainform.Controls.Remove(blockTBox);
+                mainform.Controls.Remove(previewButton);
+                mainform.Controls.Remove(removeButton);
+
+                // Remove this entry from the list
+                for (int i = id + 1; i < mainform.report.Count; i++)
+                {
+                    mainform.report[i - 1] = mainform.report[i];
+                    mainform.report[i - 1].id = i - 1;
+                    mainform.report[i - 1].UpdateLocation();
+                }
+                mainform.report.RemoveAt(mainform.report.Count - 1);
+            }
+        }
+
+        List<ReportEntry> report = new List<ReportEntry>();
+
+        public void AddRemoveButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Add/Remove button clicked");
+            AddNewEntry();
+        }
+
+        public void AddNewEntry()
+        {
+            ReportEntry newentry = new ReportEntry(report.Count);
+            report.Add(newentry);
+            newentry.UpdateLocation();
+            // newentry.removeButton.Click += new EventHandler(AddRemoveButton_Click);
+            this.Controls.Add(newentry.fileTBox);
+            this.Controls.Add(newentry.blockTBox);
+            this.Controls.Add(newentry.previewButton);
+            this.Controls.Add(newentry.removeButton);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddNewEntry();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            AddNewEntry();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = String.Format("Formula involving bug {0}.txt", bugnumTBox.Text);
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(filename, false);
+
+                for (int i = 0; i < report.Count; i++)
+                {
+                    sw.WriteLine(report[i].ToString());
+                }
+
+                sw.Close();
+                MessageBox.Show("Report created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
